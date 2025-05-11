a2d6407c6c48effcbf7dee58f8060a342a51ca69
Himaja <himajareddy1729@gmail.com>
2025-05-11 17:12:26 +0530

    model coding classes done!

---
+++ src/main/java/com/AirbnbClone/airbnb/Entity/Booking.java
@@ -0,0 +1,24 @@
+package com.AirbnbClone.airbnb.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDate startDate;
+    private LocalDate endDate;
+}
